56c3af5bf57278b550ce1a67b55147c09bf47c4a
Ian Hunter <ian@zaarly.com>
2011-09-07 16:04:58 -0400

    refactored Node#collection and added specs for template rendering per issue #6

--- spec/models/person.rb
+++ spec/models/person.rb
@@ -1,9 +1,9 @@
 
 class Person
 
-  attr_accessor :name, :age
+  attr_accessor :name, :age, :friends
 
-  def initialize(name = nil, age = nil)
-    @name, @age = name, age
+  def initialize(name = nil, age = nil, friends = nil)
+    @name, @age, @friends = name, age, friends
   end
 end
