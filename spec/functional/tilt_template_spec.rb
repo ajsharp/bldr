709e90b931b7559bb24ad51b8a897d079722fca2
Ian Hunter <ian@zaarly.com>
2011-09-09 00:03:25 -0400

    cleaned up tilt template spec, was excessive considering the node coverage

--- spec/functional/tilt_template_spec.rb
+++ spec/functional/tilt_template_spec.rb
@@ -1,83 +1,40 @@
 require 'spec_helper'
 
 describe "evaluating a tilt template" do
-
   it "registers with Tilt" do
     Tilt['test.bldr'].should == Bldr::Template
   end
 
-  describe "root object nodes" do
+  it "renders a template" do
+    alex = Person.new
+    alex.name = 'alex'
+
+    tpl = Bldr::Template.new { "object(:person => alex) { attribute(:name) }" }
+    tpl.render(Bldr::Node.new, :alex => alex).should == jsonify({:person => {:name => 'alex'}})
+  end
+
+  it "works when render two top-level objects" do
+    alex = Person.new('alex')
+    john = Person.new('john')
+
+    tpl = Bldr::Template.new {
+      <<-RUBY
+        object(:person_1 => alex) { attribute(:name) }
+        object(:person_2 => john) { attribute(:name) }
+      RUBY
+    }
+
+    result = tpl.render(Bldr::Node.new, :alex => alex, :john => john)
+    result.should == jsonify({
+      :person_1 => {:name => 'alex'},
+      :person_2 => {:name => 'john'}
+    })
+  end
+
+  describe "root Object nodes" do
 
     let(:alex) { Person.new('alex', 25) }
     let(:ian) { Person.new('ian', 32) }
-
-
-    context "with no arguments" do
-
-      describe "attribute" do
-
-        it "raises an error when only one argument is passed" do
-          tpl = %| object do
-                     attribute :url
-                   end |
-          expect {
-            it_renders_template_to_hash(tpl,{})
-          }.to raise_error(ArgumentError, "You cannot pass one argument to #attribute when inferred object is not present.")
-        end
-
-        it "raises an error when you send a lambda to an attribute with two arguments" do
-          tpl = %| object do
-                     attribute(:url,"http://foo.com") {"http://google.com"}
-                   end |
-          expect {
-            it_renders_template_to_hash(tpl,{})
-          }.to raise_error(ArgumentError, "You may only pass one argument to #attribute when using the block syntax.")
-        end
-
-        it "raises an error when you send more than 2 arguments" do
-          tpl = %| object do
-                     attribute :url, :something, :something_else
-                   end |
-          expect {
-            it_renders_template_to_hash(tpl,{})
-          }.to raise_error(ArgumentError, "You cannot pass more than two arguments to #attribute.")
-        end
-
-        it "renders two static attributes" do
-          tpl = %| object do
-                     attribute :url, "http://google.com"
-                   end |
-          it_renders_template_to_hash(tpl,{'url' => 'http://google.com'})
-        end
-
-        it "renders one attribute and one lambda" do
-          tpl = %| object do
-                     attribute(:url) {"http://google.com"}
-                   end |
-          it_renders_template_to_hash(tpl,{'url' => 'http://google.com'})
-        end
-
-      end
-
-      describe "attributes" do
-
-        it "raises an error no matter the number of arguments" do
-          expect {
-            it_renders_template_to_hash(%| object do; attributes :url end |,{})
-          }.to raise_error(ArgumentError, "You cannot use #attributes when inferred object is not present.")
-        end
-
-        # it "renders a multiple arguments" do
-        #   tpl = %| object do
-        #              attribute :url, "http://google.com"
-        #            end |
-        #   it_renders_template_to_hash(tpl,{'url' => 'http://google.com'})
-        # end
-
-      end
-
-    end
-
 
     it "returns json for a root object" do
       tpl = Bldr::Template.new {
@@ -88,7 +45,7 @@ describe "evaluating a tilt template" do
         RUBY
       }
       result = tpl.render(Bldr::Node.new, :alex => alex, :ian => ian)
-      parse_json(result).should == {'person' => {'name' => 'alex', 'age' => 25}}
+      result.should == jsonify({'person' => {'name' => 'alex', 'age' => 25}})
     end
 
     it "returns json for root object templates with nested collections" do
@@ -104,9 +61,9 @@ describe "evaluating a tilt template" do
         RUBY
       }
       result = tpl.render(Bldr::Node.new, :alex => alex, :friends => [ian])
-      parse_json(result).should == {
+      result.should == jsonify({
         'person'=> {'name' => 'alex', 'age' => 25, 'friends' => [{'name' => 'ian', 'age' => 32}]}
-      }
+      })
     end
 
   end
@@ -125,9 +82,9 @@ describe "evaluating a tilt template" do
         RUBY
       }
       result = tpl.render(Bldr::Node.new, :people => [alex,ian])
-      parse_json(result).should == {
+      result.should == jsonify({
         'people'=> [{'name' => 'alex', 'age' => 25},{'name' => 'ian', 'age' => 32}]
-      }
+      })
     end
 
     it "returns json for a root collection with embedded collection template" do
@@ -142,7 +99,7 @@ describe "evaluating a tilt template" do
         RUBY
       }
       result = tpl.render(Bldr::Node.new, :people => [alex,ian])
-      parse_json(result).should == {
+      result.should == jsonify({
         'people'=> [{
           'name' => 'alex',
           'age' => 25,
@@ -152,7 +109,7 @@ describe "evaluating a tilt template" do
           'age' => 32,
           "friends" => [{"name" => 'eric', "age" => 34}]
         }]
-      }
+      })
     end
 
   end
