ba3d23bdea59b4a28b58af3a3e3c701f960c6962
Alex Sharp <ajsharp@gmail.com>
2011-09-19 13:42:04 -0700

    Fix funky custom handler spec

--- spec/functional/handlers_spec.rb
+++ spec/functional/handlers_spec.rb
@@ -3,13 +3,11 @@ require 'spec_helper'
 describe "Defining different types of handlers" do
 
   describe "Time" do
-    context do
+    it "uses the handler for Time when rendering" do
       Bldr.handler Time do |time|
         "bar"
       end
-    end
 
-    it "uses the handler for Time when rendering" do
       output = "{\"foo\":\"bar\"}"
       node = Bldr::Node.new do
         object { attribute(:foo) { Time.now } }
