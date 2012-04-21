2b3e731085958c85fe4e90f35fe0fc0e32719fdb
Alex Sharp <ajsharp@gmail.com>
2012-04-20 18:36:24 -0700

    Update specs to work with multi_json 1.3.2

--- spec/unit/bldr_spec.rb
+++ spec/unit/bldr_spec.rb
@@ -2,17 +2,17 @@ require 'spec_helper'
 
 describe "the json encoding library" do
   it "uses yajl by default" do
-    MultiJson.engine.should == MultiJson::Engines::Yajl
+    MultiJson.engine.should == MultiJson::Adapters::Yajl
   end
 
   it "allows changing the json encoder to json pure" do
     Bldr.json_encoder = :json_pure
-    MultiJson.engine.should == MultiJson::Engines::JsonPure
+    MultiJson.engine.should == MultiJson::Adapters::JsonPure
   end
 
   it "allows changing the json encoder to the json gem" do
     Bldr.json_encoder = :json_gem
-    MultiJson.engine.should == MultiJson::Engines::JsonGem
+    MultiJson.engine.should == MultiJson::Adapters::JsonGem
   end
 
 end
