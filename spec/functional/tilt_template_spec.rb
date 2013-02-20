d784595d35b04a2ff17199bb4341708b9e575fad
Alex Sharp <ajsharp@gmail.com>
2013-02-19 17:36:45 -0700

    nest tilt template specs under Bldr module

--- spec/functional/tilt_template_spec.rb
+++ spec/functional/tilt_template_spec.rb
@@ -13,8 +13,8 @@ module Bldr
          end
         RUBY
       }.render(Node.new(nil, :parent => ctx))
-        .result
-        .should == {:person => {:name => 'john denver'}}
+      .result
+      .should == {:person => {:name => 'john denver'}}
     end
 
     it 'has access to ivars in attribute blocks with arity of 1' do
@@ -27,82 +27,82 @@ module Bldr
          end
         RUBY
       }.render(Node.new(nil, :parent => ctx))
-        .result
-        .should == {:person => {:name => 'charlie rich'}}
+      .result
+      .should == {:person => {:name => 'charlie rich'}}
     end
   end
-end
 
-describe "evaluating a tilt template" do
-  it "registers with Tilt" do
-    Tilt['test.bldr'].should == Bldr::Template
-  end
 
-  it "renders a template" do
-    alex = Person.new
-    alex.name = 'alex'
+  describe "evaluating a tilt template" do
+    it "registers with Tilt" do
+      Tilt['test.bldr'].should == Bldr::Template
+    end
 
-    tpl = Bldr::Template.new { "object(:person => alex) { attribute(:name) }" }
-    tpl.render(Bldr::Node.new, :alex => alex).result.should == {:person => {:name => 'alex'}}
-  end
+    it "renders a template" do
+      alex = Person.new
+      alex.name = 'alex'
 
-  it "allows attribute to be used at the root-level" do
-    tpl = Bldr::Template.new {
+      tpl = Bldr::Template.new { "object(:person => alex) { attribute(:name) }" }
+      tpl.render(Bldr::Node.new, :alex => alex).result.should == {:person => {:name => 'alex'}}
+    end
+
+    it "allows attribute to be used at the root-level" do
+      tpl = Bldr::Template.new {
       <<-RUBY
         attribute(:foo) { "bar" }
       RUBY
-    }
-    tpl.render(Bldr::Node.new(nil)).result.should == {:foo => 'bar'}
-  end
+      }
+      tpl.render(Bldr::Node.new(nil)).result.should == {:foo => 'bar'}
+    end
 
-  it "works when render two top-level objects" do
-    alex = Person.new('alex')
-    john = Person.new('john')
+    it "works when render two top-level objects" do
+      alex = Person.new('alex')
+      john = Person.new('john')
 
-    tpl = Bldr::Template.new {
+      tpl = Bldr::Template.new {
       <<-RUBY
         object(:person_1 => alex) { attribute(:name) }
         object(:person_2 => john) { attribute(:name) }
       RUBY
-    }
+      }
 
-    result = tpl.render(Bldr::Node.new, :alex => alex, :john => john).result
-    result.should == {
-      :person_1 => {:name => 'alex'},
-      :person_2 => {:name => 'john'}
-    }
-  end
+      result = tpl.render(Bldr::Node.new, :alex => alex, :john => john).result
+      result.should == {
+                        :person_1 => {:name => 'alex'},
+                        :person_2 => {:name => 'john'}
+                       }
+    end
 
-  it "renders nil -> null correctly" do
-    alex = Person.new('alex')
-    tpl = Bldr::Template.new {
+    it "renders nil -> null correctly" do
+      alex = Person.new('alex')
+      tpl = Bldr::Template.new {
       <<-RUBY
         object(:person_1 => alex) { attributes(:age) }
       RUBY
-    }
-    result = tpl.render(Bldr::Node.new, :alex => alex).result
-    result.should == {:person_1 => {:age => nil}}
-  end
+      }
+      result = tpl.render(Bldr::Node.new, :alex => alex).result
+      result.should == {:person_1 => {:age => nil}}
+    end
 
-  describe "root Object nodes" do
+    describe "root Object nodes" do
 
-    let(:alex) { Person.new('alex', 25) }
-    let(:ian) { Person.new('ian', 32) }
+      let(:alex) { Person.new('alex', 25) }
+      let(:ian) { Person.new('ian', 32) }
 
-    it "returns json for a root object" do
-      tpl = Bldr::Template.new {
+      it "returns json for a root object" do
+        tpl = Bldr::Template.new {
         <<-RUBY
           object :person => alex do
             attributes :name, :age
           end
         RUBY
-      }
-      result = tpl.render(Bldr::Node.new, :alex => alex, :ian => ian).result
-      result.should == {:person => {:name => 'alex', :age => 25}}
-    end
+        }
+        result = tpl.render(Bldr::Node.new, :alex => alex, :ian => ian).result
+        result.should == {:person => {:name => 'alex', :age => 25}}
+      end
 
-    it "returns json for root object templates with nested collections" do
-      tpl = Bldr::Template.new {
+      it "returns json for root object templates with nested collections" do
+        tpl = Bldr::Template.new {
         <<-RUBY
           object :person => alex do
             attributes :name, :age
@@ -112,49 +112,49 @@ describe "evaluating a tilt template" do
             end
           end
         RUBY
-      }
-      result = tpl.render(Bldr::Node.new, :alex => alex, :friends => [ian]).result
-      result.should == {
-        :person=> {:name => 'alex', :age => 25, :friends => [{:name => 'ian', :age => 32}]}
-      }
-    end
+        }
+        result = tpl.render(Bldr::Node.new, :alex => alex, :friends => [ian]).result
+        result.should == {
+                          :person=> {:name => 'alex', :age => 25, :friends => [{:name => 'ian', :age => 32}]}
+                         }
+      end
 
-    it "renders nil -> null correctly" do
-      alex = Person.new('alex')
-      tpl = Bldr::Template.new {
+      it "renders nil -> null correctly" do
+        alex = Person.new('alex')
+        tpl = Bldr::Template.new {
         <<-RUBY
           object :person_1 => alex do
             attributes(:age)
           end
         RUBY
-      }
-      result = tpl.render(Bldr::Node.new, :alex => alex).result
-      result.should == {:person_1 => {:age => nil}}
+        }
+        result = tpl.render(Bldr::Node.new, :alex => alex).result
+        result.should == {:person_1 => {:age => nil}}
+      end
+
     end
 
-  end
+    describe "root Collection nodes" do
 
-  describe "root Collection nodes" do
+      let(:alex) { Person.new('alex', 25, [Person.new('bo',33)]) }
+      let(:ian) { Person.new('ian', 32, [Person.new('eric',34)]) }
 
-    let(:alex) { Person.new('alex', 25, [Person.new('bo',33)]) }
-    let(:ian) { Person.new('ian', 32, [Person.new('eric',34)]) }
-
-    it "returns json for a root collection template" do
-      tpl = Bldr::Template.new {
+      it "returns json for a root collection template" do
+        tpl = Bldr::Template.new {
         <<-RUBY
           collection :people => people do
             attributes :name, :age
           end
         RUBY
-      }
-      result = tpl.render(Bldr::Node.new, :people => [alex,ian]).result
-      result.should == {
-        :people => [{:name => 'alex', :age => 25}, {:name => 'ian', :age => 32}]
-      }
-    end
+        }
+        result = tpl.render(Bldr::Node.new, :people => [alex,ian]).result
+        result.should == {
+                          :people => [{:name => 'alex', :age => 25}, {:name => 'ian', :age => 32}]
+                         }
+      end
 
-    it "returns json for a root collection with embedded collection template" do
-      tpl = Bldr::Template.new {
+      it "returns json for a root collection with embedded collection template" do
+        tpl = Bldr::Template.new {
         <<-RUBY
           collection :people => people do
             attributes :name, :age
@@ -163,27 +163,28 @@ describe "evaluating a tilt template" do
             end
           end
         RUBY
-      }
-      result = tpl.render(Bldr::Node.new, :people => [alex,ian]).result
-      result.should == {
-        :people=> [{
-          :name => 'alex',
-          :age => 25,
-          :friends => [{:name => 'bo', :age => 33}]
-        },{
-          :name => 'ian',
-          :age => 32,
-          :friends => [{:name => 'eric', :age => 34}]
-        }]
-      }
+        }
+        result = tpl.render(Bldr::Node.new, :people => [alex,ian]).result
+        result.should == {
+                          :people=> [{
+                                      :name => 'alex',
+                                      :age => 25,
+                                      :friends => [{:name => 'bo', :age => 33}]
+                                     },{
+                                        :name => 'ian',
+                                        :age => 32,
+                                        :friends => [{:name => 'eric', :age => 34}]
+                                       }]
+                         }
+      end
+
     end
-
   end
-end
 
-describe "using a partial template at the root of another template" do
-  it "works as expected" do
-    template = Bldr::Template.new('./spec/fixtures/root_partial.bldr')
-    template.render(Bldr::Node.new(nil, :views => './spec')).result.should == {:foo => 'bar'}
+  describe "using a partial template at the root of another template" do
+    it "works as expected" do
+      template = Bldr::Template.new('./spec/fixtures/root_partial.bldr')
+      template.render(Bldr::Node.new(nil, :views => './spec')).result.should == {:foo => 'bar'}
+    end
   end
 end
